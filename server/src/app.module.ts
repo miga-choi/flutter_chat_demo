ec20cdcc87d9e72c64ca3b9094534f9a2b41551f
miga-choi <miga.choi.1004@gmail.com>
2023-03-15 23:32:00 +0900

    app: add package "socket_io_client"

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import configuration from './config/configuration';
 import { TypeOrmConfigService } from './config/database.config';
 import { UsersModule } from './users/users.module';
@@ -21,8 +19,6 @@ import { EventsModule } from './events/events.module';
     UsersModule,
     RoomsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configServer = app.get<ConfigService>(ConfigService);
 
-  await app.listen(3000);
+  await app.listen(configServer.get<number>('http.port'));
+  app.setGlobalPrefix('api');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
